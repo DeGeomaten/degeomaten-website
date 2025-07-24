a3ebae73a1603882ba6e2aa533ca0b33a0eb518c
josephinespit <josephine.spit@hotmail.com>
2025-07-24 12:28:14 +0200

    make site

--- simplesvelte5app/vite.config.js
+++ simplesvelte5app/vite.config.js
@@ -2,9 +2,11 @@ import devtoolsJson from 'vite-plugin-devtools-json';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
+base: '/degeomaten-website/',
+	plugins: [tailwindcss(), sveltekit(), devtoolsJson(), svelte()],
 	test: {
 		projects: [
 			{
