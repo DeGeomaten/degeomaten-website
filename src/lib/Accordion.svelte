581742087866766e8367163f5392d2644881b94f
Gees Brouwer <geesbrouwer@icloud.com>
2025-08-27 16:20:38 +0200

    Accordion

---
+++ src/lib/Accordion.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  import { Accordion } from "bits-ui";
+  import CaretDown from "phosphor-svelte/lib/CaretDown";
+ 
+  const items = [
+    {
+      value: "1",
+      title: "Wie zijn 'De Geomaten'?",
+      content: "Wij zijn <b>Gees</b> en <b>Josephine</b>. Tijdens onze master Geomatics aan de TU Delft ontdekten we hoe goed onze verschillende achtergronden elkaar aanvullen – Gees met Computer Science and Engineering, Josephine met Bouwkunde. Samen delen we een nieuwsgierigheid naar hoe data en ruimte elkaar beïnvloeden, en hoe je dat zichtbaar kunt maken met kaarten en visualisaties."
+    },
+    {
+      value: "2",
+      title: "Wat doen de Geomaten?",
+      content: "De Geomaten vertalen ruimtelijke data naar duidelijke kaarten en interactieve toepassingen. We maken complexe informatie begrijpelijk en aantrekkelijk, zodat organisaties en onderzoekers sneller inzichten krijgen en beter kunnen vertellen wat hun data betekent. Of het nu gaat om een klein GIS-klusje of een creatieve visualisatie: wij zetten onze kennis en energie graag in om ruimtelijke verhalen tot leven te brengen."
+    }
+  ];
+</script>
+
+<Accordion.Root class="w-[500px] sm:max-w-[70%]" type="multiple">
+  {#each items as item (item.value)}
+    <Accordion.Item
+      value={item.value}
+      class="border-dark-10 group border-b px-1.5"
+    >
+      <Accordion.Header>
+        <Accordion.Trigger
+          class="flex w-full flex-1 select-none items-center justify-between py-5 text-[15px] font-medium transition-all [&[data-state=open]>span>svg]:rotate-180"
+        >
+          <span class="w-full text-left">
+            {item.title}
+          </span>
+          <span
+            class="hover:bg-dark-10 inline-flex size-8 items-center justify-center rounded-[7px] bg-transparent"
+          >
+            <CaretDown class="size-[18px] transition-transform duration-200" />
+          </span>
+        </Accordion.Trigger>
+      </Accordion.Header>
+      <Accordion.Content
+        class="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-sm tracking-[-0.01em]"
+      >
+        <div class="pb-[25px]">
+          {@html item.content}
+        </div>
+      </Accordion.Content>
+    </Accordion.Item>
+  {/each}
+</Accordion.Root>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+    import Accordion from "$lib/Accordion.svelte";
+    import { ArrowElbowRight, Envelope, InstagramLogo, LinkedinLogo } from "phosphor-svelte";
+</script>
+
+<div class="flex flex-col items-center justify-center min-h-screen text-center">
+    <!-- Logo -->
+    <img src="logo.svg" alt="Logo" class="w-3/13 mb-6" />
+
+    <!-- Subtitel -->
+    <h2 class="text-m font-light mb-4 text-gray-400">Geovisualisatie • GIS-oplossingen • Interactieve media</h2>
+
+    <!-- Social links -->
+    <div class="flex gap-6 mb-10 mt-8">
+        <p class="text-m font-light mb-4">
+            <ArrowElbowRight class="inline size-6 mr-2" />
+            Snel naar: </p>
+        <a href="https://www.linkedin.com/company/de-geomaten/?viewAsMember=true" target="_blank" class="text-blue-600 hover:underline">
+            <LinkedinLogo class="size-6"></LinkedinLogo>
+        </a>
+        <a href="http://instagram.com/degeomaten/" target="_blank" class="text-pink-500 hover:underline">
+            <InstagramLogo class="size-6"></InstagramLogo>
+        </a>
+        <a href="mailto:contact@degeomaten.nl" target="_blank" class="text-green-500 hover:underline">
+            <Envelope class="size-6"></Envelope>
+        </a>
+    </div>
+
+    <!-- Accordion -->
+    <Accordion />
+</div>
+
+<style>
+    * {
+        font-family: "Inter" !important;
+    }
+</style>
