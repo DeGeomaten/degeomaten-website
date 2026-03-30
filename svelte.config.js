81c27165f92844468024ecf19a5f816e3dceb1e0
Gees Brouwer <geesbrouwer@icloud.com>
2026-03-30 18:48:18 +0200

    build

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { relative, sep } from 'node:path';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -20,7 +20,9 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
     assets: 'build',
-			fallback: '404.html'
+			fallback: '404.html',
+			precompress: false,
+			strict: true
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
