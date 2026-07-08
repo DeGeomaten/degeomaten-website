4964f7e25125854c1f7239a5ca2b2a8a22ecce73
Gees Brouwer <geesbrouwer@icloud.com>
2026-07-08 18:03:32 +0200

    Revert

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import "./layout.css";
+  import favicon from "$lib/assets/favicon.png";
+
+  let { children } = $props();
+</script>
+
+<svelte:head>
+  <title>De Geomaten</title>
+  <link rel="icon" href={favicon} /></svelte:head
+>
+{@render children()}
